752959769e8316f176ec10b8b203c20d36c487ec
Kim Crayton <kmcrayton7@yahoo.com>
2015-12-11 01:46:24 +0000

    spelling error in contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
-  validates :comment, presence: true
+  validates :comments, presence: true
 end
