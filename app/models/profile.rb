2ff812fe1d176eb158a320e10a4a8a536b26957c
Kim Crayton <kmcrayton7@yahoo.com>
2015-12-14 01:56:32 +0000

    Added avatar column to Profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  
   validates :first_name, presence: true
   validates :contact_email, presence: true
   validates :bio, presence: true
@@ -7,4 +8,7 @@ class Profile < ActiveRecord::Base
   validates_length_of :coding_languages,  minimum: 1, maximum: 1000
   validates_length_of :bio,  minimum: 1, maximum: 1000
   validates_length_of :mentoring_needs,  minimum: 1, maximum: 1000
+  
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212002425) do
+ActiveRecord::Schema.define(version: 20151214015412) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20151212002425) do
     t.text     "mentoring_needs"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: true do |t|
