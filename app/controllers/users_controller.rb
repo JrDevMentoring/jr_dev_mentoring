c903b996e59ef2c59f2c4ed28d484ee0e216d5ff
Kim Crayton <kmcrayton7@yahoo.com>
2015-12-15 21:38:04 +0000

    Fixed the nav bar links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  
+  layout :choose_layout
+  
   def show
     @user = User.find( params[:id] )
   end
+  
+  private
+    def choose_layout   
+      if current_user.plan.name == 'mentor'
+       'mentor_layout'
+      elsif current_user.plan.name == 'mentee'
+       'mentee_layout'
+      end
+    end
 end
