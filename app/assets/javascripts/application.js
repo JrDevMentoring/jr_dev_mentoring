f0751c26865fe4218da46657e9d76135d0e3d016
Kim Crayton <kmcrayton7@yahoo.com>
2015-12-27 00:39:20 +0000

    Added areyousure plugin to alert user if trying to leave a page with unsaved information

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
+//= require areyousure/areyousure
 //= require_tree .
---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('form').areYouSure( {'message':'You have unsaved changes on this page. Do you want to leave this page and discard your changes or stay on this page?'});
+});
