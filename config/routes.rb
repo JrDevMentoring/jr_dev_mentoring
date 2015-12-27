32f3349ce3dd7c80cb32604497c0ad5ad8306c1e
Kim Crayton <kmcrayton7@yahoo.com>
2015-12-27 01:23:56 +0000

    Added connections route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users do
     resource :profile
   end
+  resources :connections
   resources :contacts
   get '/about' => 'pages#about'
   namespace :mentee do
