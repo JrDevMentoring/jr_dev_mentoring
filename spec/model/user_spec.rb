7990e0e85e9506fd2b497e47d529f6c84d65cb00
Kim Crayton <kmcrayton7@yahoo.com>
2016-01-25 14:11:27 -0500

    Added method test for user_spec.rb

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -50,22 +50,62 @@ describe User do
     end
   end
 
-  describe "#Mentors" do
-    it "lists all available mentors" do
+  describe "ClassMethods" do
+    before :all do
+      mentor_plan = FactoryGirl.create(:plan, :mentor)
+      mentee_plan = FactoryGirl.create(:plan, :mentee)
 
+      @mentors = FactoryGirl.create_list(:user, 3, plan: mentor_plan)
+      @mentees = FactoryGirl.create_list(:user, 4, plan: mentee_plan)
+
+      @mentors.each_with_index do |mentor, index|
+        not_available = (index == 0 ? true : false)
+        FactoryGirl.create(:profile, user: mentor, not_available: not_available)
+      end
+
+      @mentees.each_with_index do |mentee, index|
+        not_available = (index == 0 ? true : false)
+        FactoryGirl.create(:profile, user: mentee, not_available: not_available)
+      end
+
+      @available_mentors = User.mentors
+      @available_mentees = User.mentees
     end
 
-    it "doesn't list unavailable mentors" do
+    describe "#Mentors" do
+      it "lists all available mentors" do
+        expect(@available_mentors.count).to eq 2
+      end
 
+      it "doesn't list mentees" do
+        expect(@available_mentors).not_to include @mentees[0]
+      end
+
+      it "lists mentors" do
+        expect(@available_mentors).to include @mentors[1]
+      end
+
+      it "doesn't list unavailable mentors" do
+        expect(@available_mentors).not_to include @mentors[0]
+      end
+
+    end
+    describe "#Mentees" do
+      it "lists all available mentees" do
+        expect(@available_mentees.count).to eq 3
+      end
+
+      it "doesn't list mentors" do
+        expect(@available_mentees).not_to include @mentors[1]
+      end
+
+      it "lists mentors" do
+        expect(@available_mentees).to include @mentees[1]
+      end
+      it "doesn't list unavailable mentees" do
+        expect(@available_mentees).not_to include @mentees[0]
+      end
     end
   end
-  describe "#Mentees" do
-    it "lists all available mentees" do
 
-    end
-
-    it "doesn't list unavailable mentees" do
-
-    end
-  end
 end
