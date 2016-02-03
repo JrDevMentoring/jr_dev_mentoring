b9cb14e673c5461f672723ccf2db1144f8933e95
Kim Crayton <kmcrayton7@yahoo.com>
2016-02-03 17:55:08 -0500

    Fixed 'missing required :bucket option' error third attempt

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,8 +10,7 @@ class Profile < ActiveRecord::Base
   validates_length_of :bio,  minimum: 1, maximum: 1000
   validates_length_of :mentoring_needs,  minimum: 1, maximum: 500
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,
-                    :bucket => 'bucket_name'
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
+      :bucket => ENV['S3_JRDEVMENTORING'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
