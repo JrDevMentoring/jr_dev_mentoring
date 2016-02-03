32b87c561f0a9e03929cfbb715397044753586c4
Kim Crayton <kmcrayton7@yahoo.com>
2016-02-03 17:00:18 -0500

    Fixed Paperclip validation error

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -11,7 +11,7 @@ class Profile < ActiveRecord::Base
   validates_length_of :mentoring_needs,  minimum: 1, maximum: 500
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
 
 end
