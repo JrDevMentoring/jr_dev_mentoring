62379209b34b337cdafcdb9eb12c3b91c2384851
Kim Crayton <kmcrayton7@yahoo.com>
2016-02-04 08:13:04 -0500

    Added square sizing to avatar images

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,7 +10,7 @@ class Profile < ActiveRecord::Base
   validates_length_of :bio,  minimum: 1, maximum: 1000
   validates_length_of :mentoring_needs,  minimum: 1, maximum: 500
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :square => '200x200#' }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
 
