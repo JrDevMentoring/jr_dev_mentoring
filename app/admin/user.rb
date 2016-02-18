8f4b633ed8cc1acce2cfc36c0e5c53bc93f8c818
Kim Crayton <kmcrayton7@yahoo.com>
2016-02-18 11:34:02 -0500

    Updated Admin Columns

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,4 +2,15 @@ ActiveAdmin.register User do
 
  actions :index, :show, :destroy
 
+ index do
+   selectable_column
+   id_column
+   column :email
+   column :last_sign_in_at
+   column :created_at
+   column :updated_at
+   actions
+ end
+
+
 end
