602bd7909da175378ca08696e3daa5bca45aab69
Kim Crayton <kmcrayton7@yahoo.com>
2016-02-26 11:01:01 -0500

    fixed failing tests

--- spec/controllers/connections_controller_spec.rb
+++ spec/controllers/connections_controller_spec.rb
@@ -13,13 +13,19 @@ describe ConnectionsController do
   end
 
   describe 'authenticated' do
-    it 'should be able to send a connection request if signed in' do
+    it 'should not be able to send a connection request if user has no profile' do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      expect{ get :new, profile_id: @profile.id }.not_to change{ ActionMailer::Base.deliveries.count }
+      expect(response).to redirect_to 'http://test.com/sessions/new'
+    end
+
+    it 'should be able to send a connection request if user has a profile' do
       user = FactoryGirl.create(:user)
       user.profile = @profile
       user.save
       sign_in user
       expect{ get :new, profile_id: @profile.id }.to change{ ActionMailer::Base.deliveries.count }.by(1)
-
     end
 
   end
