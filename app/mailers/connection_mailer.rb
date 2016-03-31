ff45e4f1d07de68ff61f19ca1576cf08c9db3cb7
Kim Crayton <kmcrayton7@yahoo.com>
2016-03-31 17:40:29 -0400

    email

--- app/mailers/connection_mailer.rb
+++ app/mailers/connection_mailer.rb
@@ -1,11 +1,11 @@
 class ConnectionMailer < ActionMailer::Base
-  default to: 'kimmcrayton@gmail.com'
+  default to: 'adefilaedward@gmail.com'
 
   def connection_email(requester_name, requester_email, requested_name, requested_email)
     @requester_name = requester_name
     @requester_email = requester_email
     @requested_name = requested_name
     @requested_email = requested_email
-    mail(from: requester_email, to: 'adefilaedward@gmail.com', subject: 'Jr. Dev Mentoring Connect Form Message')
+    mail(from: requester_email, subject: 'Jr. Dev Mentoring Connect Form Message')
   end
 end
