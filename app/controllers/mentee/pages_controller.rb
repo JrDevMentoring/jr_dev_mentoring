1a0e7a0d090fec2609e8d66cf8c4b207d4c5896e
Kim Crayton <kmcrayton7@yahoo.com>
2016-05-23 18:05:29 -0400

    added authentication to the mentee/pages controller

--- app/controllers/mentee/pages_controller.rb
+++ app/controllers/mentee/pages_controller.rb
@@ -1,4 +1,9 @@
 class Mentee::PagesController < Mentee::MenteeController
+  before_action :authenticate_sign_in!
   def home
+  end
+
+  def authenticate_sign_in!
+    redirect_to :back unless current_user
   end
 end
