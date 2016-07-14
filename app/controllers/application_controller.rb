bc394ebe6b8fd8d3fc21fa6ad6790f1d5463b083
Sunday Adefila <adefilaedward@gmail.com>
2016-07-14 12:06:10 +0100

    added an exception handler to take care of routing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+	def routing_error(error = 'Routing error', status = :not_found, exception=nil)
+		render json: "Unknown route, Routing Error", status: 404
+	end
+
+	def action_missing(m, *args, &block)
+		Rails.logger.error(m)
+		redirect_to '/*path'
+	end
 end
 
 
