517e37759f5ed35fc10fc0d418c18304378aa706
Erin Page <erin@reax.io>
2017-01-06 20:19:15 -0600

    fixes new lines and spaces

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :contact_email, presence: true
@@ -12,6 +12,5 @@ class Profile < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :square => '200x200#' }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  
 
 end
