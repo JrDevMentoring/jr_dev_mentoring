e9b319013d58fed18b2e67e2b38d7b980edc2833
Erin Page <erin@reax.io>
2017-01-06 20:20:29 -0600

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,12 @@ ActiveRecord::Schema.define(version: 20161109170728) do
   enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace",     limit: 255
+    t.string   "namespace"
     t.text     "body"
-    t.string   "resource_id",   limit: 255, null: false
-    t.string   "resource_type", limit: 255, null: false
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.integer  "author_id"
-    t.string   "author_type",   limit: 255
+    t.string   "author_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 20161109170728) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 20161109170728) do
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
+    t.string   "name"
+    t.string   "email"
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20161109170728) do
   add_index "internships", ["user_id"], name: "index_internships_on_user_id", using: :btree
 
   create_table "plans", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -78,35 +78,35 @@ ActiveRecord::Schema.define(version: 20161109170728) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "first_name",          limit: 255
-    t.string   "last_name",           limit: 255
-    t.string   "contact_email",       limit: 255
-    t.string   "city",                limit: 255
-    t.string   "state",               limit: 255
-    t.string   "country",             limit: 255
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "contact_email"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
     t.text     "coding_languages"
     t.text     "bio"
     t.text     "mentoring_needs"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar_file_name",    limit: 255
-    t.string   "avatar_content_type", limit: 255
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.boolean  "not_available",                   default: false
+    t.boolean  "not_available",       default: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
