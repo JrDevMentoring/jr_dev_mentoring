2b3e7b0107c60fd1557f7801e2285aa40ad929c1
Erin Page <erin@reax.io>
2017-02-11 15:14:05 -0600

    check if the user has sent a connection previously before showing the button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < AuthenticationController
-  
+
   def show
     @user = User.find( params[:id] )
+    @connection = current_user.connections.find_by(requester_id: current_user.id, requested_id: @user.id)
   end
-   
+
 end
