0b23f00254d1c4fa2a51df60bba3c410eb3f9e1d
Erin Page <erin@reax.io>
2017-02-11 15:15:11 -0600

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115144700) do
+ActiveRecord::Schema.define(version: 20170211204155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 20170115144700) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "connections", force: :cascade do |t|
+    t.integer  "requester_id"
+    t.integer  "requested_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "connections", ["requested_id"], name: "index_connections_on_requested_id", using: :btree
+  add_index "connections", ["requester_id"], name: "index_connections_on_requester_id", using: :btree
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
