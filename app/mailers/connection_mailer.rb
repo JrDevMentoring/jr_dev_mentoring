3dea1ba676fe05a302a041f3b4bf31fa571dc5b3
Sunday Adefila <adefilaedward@gmail.com>
2017-02-15 20:58:52 +0100

    spec for connection model

--- app/mailers/connection_mailer.rb
+++ app/mailers/connection_mailer.rb
@@ -1,18 +1,18 @@
 class ConnectionMailer < ActionMailer::Base
 
-  def connection_email(requester_name, requester_email, requested_name, requested_email)
-    @requester_name = requester_name
-    @requester_email = requester_email
-    @requested_name = requested_name
-    @requested_email = requested_email
-    mail(from: requester_email, to: @requested_email, subject: 'Great News! Someone From Jr. Dev Mentoring Wants To Connect')
+  def connection_email(requester, requested)
+    @requester_name = requester.profile.first_name + ' ' + requester.profile.last_name
+    @requester_email = requester.email
+    @requested_name = requested.profile.first_name + ' ' + requested.profile.last_name
+    @requested_email = requested.email
+    mail(from: @requester_email, to: @requested_email, subject: 'Great News! Someone From Jr. Dev Mentoring Wants To Connect')
   end
 
-  def connection_email_copy(requester_name, requester_email, requested_name, requested_email)
-    @requester_name = requester_name
-    @requester_email = requester_email
-    @requested_name = requested_name
-    @requested_email = requested_email
-    mail(from: requester_email, to: 'kmc3046@gmail.com', subject: 'Jr. Dev Mentoring Connect Form Message')
+  def connection_email_copy(requester, requested)
+    @requester_name = requester.profile.first_name + ' ' + requester.profile.last_name
+    @requester_email = requester.email
+    @requested_name = requested.profile.first_name + ' ' + requested.profile.last_name
+    @requested_email = requested.email
+    mail(from: @requester_email, to: 'kmc3046@gmail.com', subject: 'Jr. Dev Mentoring Connect Form Message')
   end
 end
