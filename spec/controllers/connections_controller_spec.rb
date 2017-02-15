5669e6366ae5bc452ea89a15124bd8eab1275345
Sunday Adefila <adefilaedward@gmail.com>
2017-02-15 21:16:15 +0100

    cleaned up connections controller, improved specs

--- spec/controllers/connections_controller_spec.rb
+++ spec/controllers/connections_controller_spec.rb
@@ -1,31 +1,43 @@
 require 'rails_helper'
 
 describe ConnectionsController do
+
+  let(:profile) { create(:profile) }
+  let(:connection_request) { get :new, profile_id: profile.id }
+
   before do
     @request.env['HTTP_REFERER'] = 'http://test.com/sessions/new'
-    @profile = FactoryGirl.create(:profile)
   end
+
   describe 'unauthenticated' do
     it 'should not be able to send a connection request if not signed in' do
-      expect{get :new, profile_id: @profile.id }.not_to change{ ActionMailer::Base.deliveries.count }
+      expect{ connection_request }.not_to change{ ActionMailer::Base.deliveries.count }
     end
-
   end
 
   describe 'authenticated' do
     it 'should not be able to send a connection request if user has no profile' do
-      user = FactoryGirl.create(:user)
+      user = create(:user)
       sign_in user
-      expect{ get :new, profile_id: @profile.id }.not_to change{ ActionMailer::Base.deliveries.count }
+      expect{ connection_request }.not_to change{ ActionMailer::Base.deliveries.count }
       expect(response).to redirect_to 'http://test.com/sessions/new'
     end
 
-    it 'should be able to send a connection request if user has a profile' do
-      user = FactoryGirl.create(:user)
-      user.profile = @profile
-      user.save
-      sign_in user
-      expect{ get :new, profile_id: @profile.id }.to change{ ActionMailer::Base.deliveries.count }.by(2)
+    context 'with completed params' do
+      before do
+        @user = create(:profile).user
+        sign_in @user
+      end
+
+      it 'should be able to send a connection request if user has a profile' do
+        profile = @user.profile
+        expect{ connection_request }.to change{ ActionMailer::Base.deliveries.count }.by 2
+      end
+
+      it 'should create a new connection' do
+        profile = @user.profile
+        expect{ connection_request }.to change{ Connection.count }.by 1
+      end
     end
 
   end
