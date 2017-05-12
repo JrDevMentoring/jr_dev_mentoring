7a5f47cb644907b3e2d62e0988e106abb33b6a9c
Jennifer Konikowski <jmkoni@icloud.com>
2017-05-12 15:56:45 -0400

    [#42] add search by programming language to Communities#index (#44)
    Add DatabaseCleaner to both profile and user spec to clear out DB between tests.
    Moved mentee/mentor to Profile since Communities#index was primarily returning profiles
    and because the search was on Profile, not User. Added pg_search gem to allow for text
    search since, currently, languages are a text field, not a drop down selection.

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,11 @@
 class CommunitiesController < AuthenticationController
   def index
-    @available_mentors = User.mentors if current_user.mentee?
-    @available_mentees = User.mentees if current_user.mentor?
+    if params[:query].present?
+      @available_mentors = Profile.mentors.search_for(params[:query]) if current_user.mentee?
+      @available_mentees = Profile.mentees.search_for(params[:query]) if current_user.mentor?
+    else
+      @available_mentors = Profile.mentors if current_user.mentee?
+      @available_mentees = Profile.mentees if current_user.mentor?
+    end
   end
 end
